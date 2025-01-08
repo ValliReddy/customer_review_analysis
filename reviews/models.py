d2e3f96e7035617b8074b3ec4cdfe19b8f8b4472
ValliReddy <157823804+ValliReddy@users.noreply.github.com>
2025-01-08 02:50:11 -0800

    creating admin

--- reviews/models.py
+++ reviews/models.py
@@ -24,7 +24,7 @@ class User(AbstractBaseUser):
     password = models.CharField(max_length=250)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-
+    is_staff = models.BooleanField(default=False)  # Add this line
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
